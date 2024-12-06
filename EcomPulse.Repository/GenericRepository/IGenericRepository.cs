1619167726877973c43b107bae286b81e1ac25b3
SemihKaraagacli <karaagaclisemih@gmail.com>
2024-12-06 23:56:10 +0300

    GenericRepository created

---
+++ EcomPulse.Repository/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace EcomPulse.Repository.GenericRepository
+{
+    public interface IGenericRepository<T>
+    {
+        Task<IEnumerable<T>> GetAsync();
+        Task<T> GetByIdAsync(Guid Id);
+        void CreateAsync(T Entity);
+        void UpdateAsync(T Entity);
+        void DeleteAsync(T Entity);
+        Task<IEnumerable<T>> WhereAsync(Expression<Func<T, bool>> predicate);
+
+    }
+}
