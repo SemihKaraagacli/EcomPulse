a6084837b52380ba87456007acf49294b0231afb
SemihKaraagacli <karaagaclisemih@gmail.com>
2024-12-07 16:15:25 +0300

    CategoryRepository Added

---
+++ EcomPulse.Repository/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using EcomPulse.Repository.Entities;
+using EcomPulse.Repository.GenericRepository;
+
+namespace EcomPulse.Repository.CategoryRepository
+{
+    public class CategoryRepository(AppDbContext context) : GenericRepository<Category>(context), ICategoryRepository
+    {
+    }
+}
