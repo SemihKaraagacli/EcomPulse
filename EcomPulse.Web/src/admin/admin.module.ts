ed793e945d54f1f8a49a8e03962da05c4041333e
SemihKaraagacli <karaagaclisemih@gmail.com>
2024-12-30 13:56:05 +0300

    Signin pages created

--- EcomPulse.Web/src/admin/admin.module.ts
+++ EcomPulse.Web/src/admin/admin.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   { path: '', component: AdminComponent },
+  { path: 'signin', component: SigninComponent },
   // Diğer admin rotaları burada tanımlanabilir
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
 
 @NgModule({
-  imports: [AdminRoutingModule,AdminComponent]
+  imports: [AdminRoutingModule, AdminComponent],
 })
-export class AdminModule { }
+export class AdminModule {}
