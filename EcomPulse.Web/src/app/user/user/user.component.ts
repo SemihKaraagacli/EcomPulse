182759526b7290c70d28427643c271bfee662650
SemihKaraagacli <karaagaclisemih@gmail.com>
2025-01-23 22:27:42 +0300

    Admin product page listened. #32

--- EcomPulse.Web/src/app/user/user/user.component.ts
+++ EcomPulse.Web/src/app/user/user/user.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, Type } from '@angular/core';
 import { ProductComponent } from '../product/product.component';
 import { AuthService } from '../../shared/service/auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, RouterModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
